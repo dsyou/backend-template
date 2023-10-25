0e9487c6ecd07b68e40be076116f9381240bf7c1
Dawid Janik <daw_janik@protonmail.com>
2023-10-25 11:51:24 +0200

    Added common ddd annotations.

--- core/src/main/java/pl/dsyou/TemplateApplication.java
+++ core/src/main/java/pl/dsyou/TemplateApplication.java
@@ -1,4 +1,4 @@
-package pl.dsyou.template;
+package pl.dsyou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- core/src/test/java/pl/dsyou/TemplateApplicationTest.java
+++ core/src/test/java/pl/dsyou/TemplateApplicationTest.java
@@ -1,13 +1,13 @@
-package pl.dsyou.template;
+package pl.dsyou;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TemplateApplicationTests {
+class TemplateApplicationTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
